521560e6a0ec8b6c3f04849d3d5ec144366b7382
RajVivek <41441461+VIVEKRAJ23@users.noreply.github.com>
2022-03-27 14:01:11 +0530

    Update Age Calculator.py

--- Age Calculator.py
+++ Age Calculator.py
@@ -22,29 +22,29 @@ def get_age():
 import tkinter as tk
 window = tk.Tk()
 window.geometry("400x300")
-window.config(bg="#F7DC6F")
+window.config(bg="#138D75")
 window.resizable(width=False,height=False)
 window.title('Age Calculator!')
  
-l1 = tk.Label(window,text="The Age Calculator!",font=("Arial", 20),fg="black",bg="#F7DC6F")
-l2 = tk.Label(window,font=("Arial",12),text="Enter your birthday which includes the day-month-year.",fg="black",bg="#F7DC6F")
+l1 = tk.Label(window,text="The Age Calculator",font=("Arial", 20,"bold"),fg="black",bg="#138D75")
+l2 = tk.Label(window,font=("Arial",12,"bold"),text="Enter your birthday",fg="black",bg="#138D75")
  
-l_d=tk.Label(window,text="Date: ",font=('Arial',12,"bold"),fg="darkgreen",bg="#F7DC6F")
-l_m=tk.Label(window,text="Month: ",font=('Arial',12,"bold"),fg="darkgreen",bg="#F7DC6F")
-l_y=tk.Label(window,text="Year: ",font=('Arial',12,"bold"),fg="darkgreen",bg="#F7DC6F")
+l_d=tk.Label(window,text="Date: ",font=('Arial',12,"bold"),fg="darkgreen",bg="#138D75")
+l_m=tk.Label(window,text="Month: ",font=('Arial',12,"bold"),fg="darkgreen",bg="#138D75")
+l_y=tk.Label(window,text="Year: ",font=('Arial',12,"bold"),fg="darkgreen",bg="#138D75")
 e1=tk.Entry(window,width=5)
 e2=tk.Entry(window,width=5)
 e3=tk.Entry(window,width=5)
  
-b1=tk.Button(window,text="Calculate Age!",font=("Arial",13),command=get_age)
+b1=tk.Button(window,text="Calculate Age",font=("Arial",13),command=get_age)
  
-l3 = tk.Label(window,text="The Calculated Age is: ",font=('Arial',12,"bold"),fg="darkgreen",bg="#F7DC6F")
+l3 = tk.Label(window,text="The Calculated Age is: ",font=('Arial',12,"bold"),fg="darkgreen",bg="#138D75")
 t1=tk.Text(window,width=5,height=0,state="disabled")
  
-b2=tk.Button(window,text="Exit Application!",font=("Arial",13),command=exit)
+b2=tk.Button(window,text="Exit Application",font=("Arial",13),command=exit)
  
 l1.place(x=70,y=5)
-l2.place(x=10,y=40)
+l2.place(x=100,y=40)
 l_d.place(x=100,y=70)
 l_m.place(x=100,y=95)
 l_y.place(x=100,y=120)
@@ -57,10 +57,3 @@ t1.place(x=240,y=203)
 b2.place(x=100,y=230)
  
 window.mainloop()
-
-
-# In[ ]:
-
-
-
-
